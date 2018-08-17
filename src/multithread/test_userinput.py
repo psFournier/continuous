87281fb6734536a47e8e0f3a53263f391b5a6c34
pifouuu <pierre00fournier@gmail.com>
2018-08-17 16:23:00 +0200

    training without tutor seems to be working with threads

--- src/multithread/test_userinput.py
+++ src/multithread/test_userinput.py
@@ -8,24 +8,23 @@ import tensorflow as tf
 import numpy as np
 from collections import deque
 import keras.backend as K
+import sys
 
 class Agent():
-    def __init__(self, sess, s_dim, num_actions, lr):
-        self.sess = sess
+    def __init__(self, s_dim, num_actions, lr):
         self.step = 0
         self.epStep = 0
         self.run_thread = Thread(target=self.run)
-        self.tutor_thread = Thread(target=self.input)
+        self.tutor_thread = Thread(target=self.input, daemon=True)
         self.sDim = s_dim
         self.num_actions = num_actions
         self.learning_rate = lr
-        self.names = ['state0', 'action', 'feedback', 'weight']
+        self.names = ['state0', 'action', 'feedback', 'fWeight']
         self.buffer = ReplayBuffer(limit=int(1e6), names=self.names)
         self.batchSize = 64
         self.episode = deque(maxlen=400)
 
     def create_model(self):
-        K.set_session(self.sess)
         state = Input(shape=self.sDim)
         action = Input(shape=(1,), dtype='uint8')
         l1 = Dense(400, activation="relu")(state)
@@ -40,46 +39,40 @@ class Agent():
         return feedbackModel
 
     def train(self):
+        loss = 0
         if self.buffer.nb_entries > self.batchSize:
             samples = self.buffer.sample(self.batchSize)
             s, a, targets, weights = [np.array(samples[name]) for name in self.names]
-            self.loss = self.model.train_on_batch(x=[s,a], y=targets, sample_weight=weights)
+            loss = self.model.train_on_batch(x=[s,a], y=targets, sample_weight=weights)
+        return loss
 
     def start(self):
         self.run_thread.start()
         self.tutor_thread.start()
-
-    # def init_variables(self):
-    #     variables = tf.global_variables()
-    #     uninitialized_variables = []
-    #     for v in variables:
-    #         if not hasattr(v, '_keras_initialized') or not v._keras_initialized:
-    #             uninitialized_variables.append(v)
-    #             v._keras_initialized = True
-    #     self.sess.run(tf.variables_initializer(uninitialized_variables))
+        self.run_thread.join()
 
     def run(self):
         self.model = self.create_model()
-        # self.init_variables()
         state0 = np.random.randint(0, 4, size=(5,))
         while self.step < 100000:
             action = np.random.randint(self.num_actions)
             state1 = np.random.randint(0, 4, size=(5,))
             self.step += 1
             self.epStep += 1
-            experience = {'state0': state0, 'action': action, 'weight': 0, 'feedback': np.random.choice([1,-1])}
+            experience = {'state0': state0, 'action': action, 'fWeight': 1, 'feedback': 1}
             self.episode.append(experience)
-            self.train()
+            self.loss = self.train()
+
             state0 = state1
             if self.epStep >= 200:
                 for s in range(self.epStep):
                     exp = self.episode.popleft()
-                    if exp['weight'] != 0:
+                    if exp['fWeight'] != 0:
                         self.buffer.append(exp)
                 self.epStep = 0
                 state0 = np.random.randint(0, 4, size=(5,))
             if self.step % 200 == 0:
-                print(self.step)
+                print(self.step, self.loss)
 
     def input(self):
         while True:
@@ -99,5 +92,5 @@ if __name__ == '__main__':
     num_actions = 5
     lr = 0.01
     with tf.Session() as sess:
-        agent = Agent(sess, s_dim, num_actions, lr)
+        agent = Agent(s_dim, num_actions, lr)
         agent.start()
