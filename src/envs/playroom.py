28fc95e1ce41fc4acbd5c6e15e797bf9c72d4649
pifouuu <pierre00fournier@gmail.com>
2018-09-18 18:45:48 +0200

    new playroom with four rooms and chests and keys...

--- src/envs/playroom.py
+++ src/envs/playroom.py
@@ -8,15 +8,18 @@ from gym import Env
         self.chest3 = Chest3(self, 4, 6)'''
 
 MAP = [
-    "+-------------+",
-    "|    _ _|     |",
-    "|       :     |",
-    "|_ _ _  |     |",
-    "|    _| |     |",
-    "|    _ _|   | |",
-    "|       |   | |",
-    "|       |   | |",
-    "+-------------+",
+    "+-------------------+",
+    "|         |         |",
+    "|                   |",
+    "|                   |",
+    "|                   |",
+    "|_       _|_       _|",
+    "|         |         |",
+    "|                   |",
+    "|                   |",
+    "|                   |",
+    "|         |         |",
+    "+-------------------+",
 ]
 
 class Actions:
@@ -33,101 +36,109 @@ class Actions:
     # PUT = 10
 
 class Obj():
-    def __init__(self, env, x, y):
+    def __init__(self, env, x, y, name):
         self.x = x
         self.y = y
         self.s = 0
         self.in_hand = 0
         self.env = env
+        self.name = name
+        self.env.objects.append(self)
 
     def act(self, a):
         pass
 
-    @property
-    def light(self):
-        return self.env.light.s == 1
-
 class Light(Obj):
-    def __init__(self, env, x, y):
-        super(Light, self).__init__(env, x, y)
-        self.name = 'light'
+    def __init__(self, env, x, y, name):
+        super(Light, self).__init__(env, x, y, name)
 
     def act(self, a):
         if a==Actions.TOUCH:
             self.s = 1 - self.s
 
     @property
-    def smax(self):
-        return 1
+    def high(self):
+        return [1]
 
-class Key1(Obj):
-    def __init__(self, env, x, y):
-        super(Key1, self).__init__(env, x, y)
-        self.name = 'key1'
+    @property
+    def low(self):
+        return [0]
+
+    @property
+    def state(self):
+        return [self.s]
+
+    @property
+    def init(self):
+        return [0]
+
+class Key(Obj):
+    def __init__(self, env, x, y, name, light, prop):
+        super(Key, self).__init__(env, x, y, name)
+        self.light = light
+        self.prop = prop
 
     def act(self, a):
         if a == Actions.TAKE:
-            if self.light:
-                self.in_hand = 1
+            if self.light.s == 1:
+                self.in_hand = np.random.choice([0, 1], p=self.prop)
 
     @property
-    def smax(self):
-        return 0
-
-class Chest1(Obj):
-    def __init__(self, env, x, y):
-        super(Chest1, self).__init__(env, x, y)
-        self.name = 'chest1'
-
-    def act(self, a):
-        if a == Actions.TOUCH:
-            if self.light:
-                self.s = np.random.choice(range(4), p=[0.2, 0.4, 0.2, 0.2])
+    def state(self):
+        return [self.in_hand]
 
     @property
-    def smax(self):
-        return 3
+    def high(self):
+        return [1]
 
-class Chest2(Obj):
-    def __init__(self, env, x, y):
-        super(Chest2, self).__init__(env, x, y)
-        self.name = 'chest2'
+    @property
+    def low(self):
+        return [0]
+
+    @property
+    def init(self):
+        return [0]
+
+class Chest(Obj):
+    def __init__(self, env, x, y, name, light, key, prop):
+        super(Chest, self).__init__(env, x, y, name)
+        self.light = light
+        self.prop = prop
+        self.key = key
 
     def act(self, a):
-        if self.light:
+        if self.light.s == 1 and self.key.in_hand == 1:
             if a == Actions.TOUCHDOWN:
                 if self.s == 0:
-                    self.s = 1
+                    self.s = np.random.choice([0, 1], p=self.prop)
             elif a == Actions.TOUCHUP:
                 if self.s == 1:
-                    self.s = 2
+                    self.s = np.random.choice([1, 2], p=self.prop)
             elif a == Actions.TOUCHLEFT:
                 if self.s == 2:
-                    self.s = 3
+                    self.s = np.random.choice([2, 3], p=self.prop)
             elif a == Actions.TOUCHRIGHT:
                 if self.s == 3:
-                    self.s = 4
+                    self.s = np.random.choice([3, 4], p=self.prop)
             elif a == Actions.TOUCH:
                 if self.s > 0:
                     self.s = 0
 
     @property
-    def smax(self):
-        return 4
-
-class Chest3(Obj):
-    def __init__(self, env, x, y):
-        super(Chest3, self).__init__(env, x, y)
-        self.name = 'chest3'
-
-    def act(self, a):
-        if a == Actions.TOUCH:
-            if self.light:
-                self.s = 1
+    def high(self):
+        return [4]
 
     @property
-    def smax(self):
-        return 1
+    def low(self):
+        return [0]
+
+    @property
+    def state(self):
+        return [self.s]
+
+    @property
+    def init(self):
+        return [0]
 
 class Playroom(Env):
 
@@ -137,22 +148,27 @@ class Playroom(Env):
         self.desc = np.asarray(MAP,dtype='c')
         self.maxR = 6
         self.maxC = 6
-        self.init()
+        self.initialize()
 
-    def init(self):
+    def initialize(self):
         self.x = 0
         self.y = 0
-        self.light = Light(self, 2, 3)
-        self.key1 = Key1(self, 0, 3)
-        self.chest1 = Chest1(self, 3, 2)
-        self.chest2 = Chest2(self, 5, 2)
-        self.chest3 = Chest3(self, 4, 6)
-        self.objects = [self.key1, self.chest1, self.chest2, self.chest3, self.light]
+        self.objects = []
+        self.light1 = Light(self, 0, 4, 'light1')
+        self.light2 = Light(self, 0, 9, 'light2')
+        self.light3 = Light(self, 5, 4, 'light3')
+        self.light4 = Light(self, 5, 9, 'light4')
+        self.key1 = Key(self, 4, 0, 'key1', self.light1, [0, 1])
+        self.key2 = Key(self, 4, 5, 'key2', self.light1, [0.2, 0.8])
+        self.key3 = Key(self, 9, 0, 'key3', self.light1, [0.4, 0.6])
+        self.key4 = Key(self, 9, 5, 'key4', self.light1, [0.6, 0.4])
+        self.chest1 = Chest(self, 2, 2, 'chest1', self.light1, self.key1, [0, 1])
+        self.chest2 = Chest(self, 2, 7, 'chest2', self.light2, self.key2, [0.2, 0.8])
+        self.chest3 = Chest(self, 7, 2, 'chest3', self.light3, self.key3, [0.4, 0.6])
+        self.chest3 = Chest(self, 7, 7, 'chest4', self.light4, self.key4, [0.6, 0.4])
         self.lastaction = None
 
     def step(self, a):
-
-        # print(self.get_state(), a)
 
         if a==Actions.UP and self.desc[1 + self.x, 1 + 2 * self.y] == b" ":
             self.x = min(self.x + 1, self.maxR)
@@ -240,35 +256,41 @@ class Playroom(Env):
         return -1
 
     def reset(self):
-        self.init()
+        self.initialize()
         return np.array(self.state)
 
     @property
-    def state_high(self):
-        smax = [self.maxR , self.maxC]
-        for o in self.objects:
-            smax += [self.maxR, self.maxC, o.smax, 1]
-        return smax
+    def high(self):
+        res = [9, 9]
+        for obj in self.objects:
+            res += obj.high
+        return res
 
     @property
     def state(self):
         res = [self.x, self.y]
         for obj in self.objects:
-            res += [obj.x, obj.y, obj.s, obj.in_hand]
+            res += obj.state
         return res
 
     @property
-    def state_init(self):
-        return [0,0,0,3,0,0,3,2,0,0,5,2,0,0,4,6,0,0,2,3,0,0]
+    def init(self):
+        res = [0, 0]
+        for obj in self.objects:
+            res += obj.init
+        return res
 
     @property
-    def state_low(self):
-        return [0] * (2 + len(self.objects) * 4)
+    def low(self):
+        res = [0, 0]
+        for obj in self.objects:
+            res += obj.low
+        return res
 
 if __name__ == '__main__':
     env = Playroom()
     env.reset()
-    for a in [0, 0, 2, 2, 2, 4, 0, 0, 0,3,3,3,1,2,2,4, 4, 4, 4, 4]:
+    for a in [2,2,2,2,4,3,3,3,3,0,0,0,0,9,1,1,2,2,2,6,2,2]:
         # a = np.random.randint(11)
         env.step(a)
 
